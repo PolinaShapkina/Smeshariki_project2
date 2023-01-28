839df3fa65de1ce1c846fcd1db85b9baae2c6934
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-01-28 23:36:49 +0300

    МАРШРУТИЗАТОР

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,27 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Страница Кроша */
+router.get('/krosh', function(req, res, next) {
+  res.send("<h1>Страница Кроша</h1>")
+  });
+  /* Страница Нюши */
+router.get('/nyusha', function(req, res, next) {
+  res.send("<h1>Страница Нюши</h1>")
+  });
+  /* Страница Пина */
+  router.get('/pin', function(req, res, next) {
+  res.send("<h1>Страница Пина</h1>")
+  });
+  /* Страница Копатыча */
+  router.get('/kopatych', function(req, res, next) {
+  res.send("<h1>Страница Копатыча</h1>")
+  });
+  /* Страница Лосяша */
+  router.get('/losyash', function(req, res, next) {
+  res.send("<h1>Страница Лосяша</h1>")
+  });
+  
 
 module.exports = router;
+
