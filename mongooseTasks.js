eeccdad9a6fa627ac4d34e58c83808cb1e7059f9
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-01-29 22:46:29 +0300

    ORM

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/')
+mongoose.connect('mongodb://127.0.0.1:27017/test')
 
 var heroes = mongoose.model('heroes', { name: String })
 
