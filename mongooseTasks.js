0a7ac578188e3d4629cbf219c36a0e6b12379dfe
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-01-29 22:54:03 +0300

    СОЗДАНИЕ СХЕМЫ

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-var heroes = mongoose.model('heroes', { name: String })
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.lala = function(){
+    console.log(this.get("name") + " сказал привет")
+}
+
+var heroes = mongoose.model('heroes', schema)
 
 var hero = new heroes({ name: 'Крош' })
 hero.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('Hey')
-    }
+    hero.lala()
 })
