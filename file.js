448321417c187cef6d28713ec1e855cdecf9ff53
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-01-29 22:38:02 +0300

    ШАБЛОН WATERFALL

--- file.js
+++ file.js
@@ -1,18 +1,12 @@
-async.parallel([
-    firstWord,
-    secondWord,
-    thirdWord
+async.waterfall([
+    function(callback){
+    callback(null, "МАМА", "МЫЛА","РАМУ")
+    },
+    function(arg1, arg2, arg3, callback){
+    callback(null, arg1 + ' ' + arg2 + ' ' + arg3)
+    }
     ],
-    function(err,result){
-    if(err) throw err;
-    console.log(result.join(' '))
+    function(err, result){
+    if(err) throw err
+    console.log(result)
     });
-    function firstWord(callback){
-    callback(null, 'МАМА')
-    }
-    function secondWord(callback){
-    callback(null, 'МЫЛА')
-    }
-    function thirdWord(callback){
-    callback(null, 'РАМУ')
-    }
