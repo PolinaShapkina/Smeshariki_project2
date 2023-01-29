c0fa53e72755e99118ef5eec98a486bf62c6a1d9
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-01-29 22:34:39 +0300

    ШАБЛОН SERIES

---
+++ mongooseTasks.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/')
+
+var heroes = mongoose.model('heroes', { name: String })
+
+var hero = new heroes({ name: 'Крош' })
+hero.save(function (err) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Hey')
+    }
+})
