0034c912cec8c9c7546e16eb68728472d9dc1f2b
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-01-29 23:33:54 +0300

    ПОДКЛЮЧЕНИЕ БАЗЫ ДАННЫХ

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
