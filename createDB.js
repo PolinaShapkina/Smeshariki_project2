c817f46a3fc892cf00cb31acea2b789056c150bd
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-01-30 23:18:50 +0300

    исправление ошибок

--- createDB.js
+++ createDB.js
@@ -1,4 +1,5 @@
 var MongoClient = require('mongodb').MongoClient
+var data = require("./data.js").data
 
 const uri = "mongodb://127.0.0.1:27017/"
 const client = new MongoClient(uri)
@@ -9,10 +10,24 @@ var database = client.db("heroes");
 database.dropDatabase()
 database = client.db("heroes");
 const hunter = database.collection("hero");
-const result = await hunter.insertOne({name:"Крош"});
-console.log(`${result} documents were inserted`);
+const result = await hunter.insertMany(data);
+console.log(`${result.insertedCount} documents were inserted`);
+for (const key in result) {
+            console.log(`${key}: ${result[key]}`);
+            }
 } finally {
 await client.close();
 }
 }
 run()
+/*var mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/')
+var Hero = require("./models/hero").Hero
+var hero = new Hero({
+title: "Крош",
+nick: "krosh"
+})
+console.log(hero)
+hero.save(function(){
+console.log(arguments)
+})*/
